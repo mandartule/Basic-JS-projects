529608851239c4d90799b268c2d3021ce7afe7d8
Mandar Tule <mandartule5@gmail.com>
2023-10-24 11:56:10 +0530

    add

--- pokemons/app.js
+++ pokemons/app.js
@@ -3,8 +3,15 @@ let section = document.querySelector('#container');
 
 
 for (let i = 10001; i <= 10099; i++) {
+    let div = document.createElement('div');
+    let span = document.createElement('span');
+    span.innerText = `#${i-10000}`;
     let img = document.createElement('img');
     img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
-    section.appendChild(img);
+    div.appendChild(img);
+    div.appendChild(span);
+    div.classList.add('pokemon');
+   
+    section.appendChild(div);
 
 }
