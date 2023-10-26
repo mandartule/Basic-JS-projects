fcd306c89a33e21c4679e6d3cd4d535dd1a3bd0d
Mandar Tule <mandartule5@gmail.com>
2023-10-27 00:17:15 +0530

    new files

---
+++ formSubmit/app.js
@@ -0,0 +1,10 @@
+let form = document.querySelector('#form');
+let input = document.querySelector('#input');
+let ul = document.querySelector('#type')
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    let li = document.createElement('LI');
+    li.innerText = input.value;
+    ul.appendChild(li);
+})
